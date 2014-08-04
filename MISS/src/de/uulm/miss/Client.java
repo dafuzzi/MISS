0ceaf95b35950853331f59322130c7a76dc082b2
dafuzzi <Fuzzi@eduroam161-038.wlan.uni-ulm.de>
2014-08-04 15:51:08 +0200

    added joda time and base classes
    client station and parser

--- MISS/src/de/uulm/miss/Client.java
+++ MISS/src/de/uulm/miss/Client.java
@@ -1,4 +1,4 @@
-package com.example.testservice;
+package de.uulm.miss;
 
 import org.joda.time.DateTime;
 
@@ -11,12 +11,12 @@ public class Client {
 	private int packets;
 	private String BSSID;
 	private String ESSID;
-    
+
 	public Client(String customName, String MAC) {
 		this.customName = customName;
 		this.MAC = MAC;
 	}
-    
+
 	public Client(String customName, String MAC, DateTime firstTimeSeen, DateTime lastTimeSeen, int power, int packets, String bSSID, String eSSID) {
 		this.customName = customName;
 		this.MAC = MAC;
@@ -30,65 +30,65 @@ public class Client {
 	public String getCustomName() {
 		return customName;
 	}
-    
+
 	public void setCustomName(String customName) {
 		this.customName = customName;
 	}
-    
+
 	public String getMAC() {
 		return MAC;
 	}
-    
+
 	public void setMAC(String MAC) {
 		this.MAC = MAC;
 	}
-    
+
 	public DateTime getFirstTimeSeen() {
 		return firstTimeSeen;
 	}
-    
+
 	public void setFirstTimeSeen(DateTime firstTimeSeen) {
 		this.firstTimeSeen = firstTimeSeen;
 	}
-    
+
 	public DateTime getLastTimeSeen() {
 		return lastTimeSeen;
 	}
-    
+
 	public void setLastTimeSeen(DateTime lastTimeSeen) {
 		this.lastTimeSeen = lastTimeSeen;
 	}
-    
+
 	public int getPower() {
 		return power;
 	}
-    
+
 	public void setPower(int power) {
 		this.power = power;
 	}
-    
+
 	public int getPackets() {
 		return packets;
 	}
-    
+
 	public void setPackets(int packets) {
 		this.packets = packets;
 	}
-    
+
 	public String getBSSID() {
 		return BSSID;
 	}
-    
+
 	public void setBSSID(String bSSID) {
 		BSSID = bSSID;
 	}
-    
+
 	public String getESSID() {
 		return ESSID;
 	}
-    
+
 	public void setESSID(String eSSID) {
 		ESSID = eSSID;
 	}
-    
+
 }
